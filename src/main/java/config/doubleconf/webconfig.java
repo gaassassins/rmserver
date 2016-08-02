8a30bcdf7b655ba0699c48f43c7351d008c3aa39
Maria Malyarova <savoreux69@gmail.com>
2016-08-02 12:52:00 +0300

    added skeleton for server

---
+++ src/main/java/config/doubleconf/webconfig.java
@@ -0,0 +1,13 @@
+package config.doubleconf;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("remidemeserver")
+
+public class Webconfig extends WebMvcConfigurerAdapter{
+}
